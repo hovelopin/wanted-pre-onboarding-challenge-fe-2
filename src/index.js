37e65c9e2c51c79b3de729448cab479c69ab7de2
hovelopin <kimhojin3714@naver.com>
2023-01-30 18:44:50 +0900

    [TS-1] feat : jsdoc 초기 셋팅 완료

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * @function addition
+ * @param {number} a 연산하고자 하는 0 이상의 정수
+ * @param {number} b 연산하고자 하는 0 이상의 정수
+ * @returns {number} a + b
+ */
+
+const addition = (a, b) => a + b;
+
+/**
+ * @function substraction
+ * @param {number} a 연산하고자 하는 0 이상의 정수
+ * @param {number} b 연산하고자 하는 0 이상의 정수
+ * @returns {number} a - b
+ */
+const substraction = (a, b) => a - b;
---
+++ src/multiply.js
@@ -0,0 +1,8 @@
+/**
+ * @function multiply
+ * @param {number} a 연산하고자 하는 0 이상의 정수
+ * @param {number} b 연산하고자 하는 0 이상의 정수
+ * @returns {number} a * b
+ */
+
+const multiply = (a, b) => a * b;
